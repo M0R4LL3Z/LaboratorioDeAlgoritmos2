81b76bc6a4ab3c7c972c6603ba4886eb52e11e40
M0R4LL3Z <gdamacena7@gmail.com>
2023-03-16 15:51:35 -0300

    Exercicios

---
+++ Aula01/main.py
@@ -0,0 +1,9 @@
+#1.Ler uma lista
+#de 5 números inteiros e mostre cada número juntamente com a sua posição na
+#lista;
+
+list =[1,2,3,4,5]
+c = -1
+for i in list:
+    c = c + 1
+    print(f'Número {i}, posição {c}')
